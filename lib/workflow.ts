6d4e432b3272c2719f1b18aaff915108cba71d2c
Christian Tallgren <ctallgren@outlook.com>
2025-01-28 13:45:51 +0200

    implement qstash workflows for user onboarding

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,35 @@
 import { Client as Workflowclient } from "@upstash/workflow";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 import config from "./config";
 
 export const workflowclient = new Workflowclient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Tallgren Dev <hello.tallgren.dev>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
